ad2fdb12ff7de079cc4822dd11e6c4bb94f2d9ad
Alexandar <AlexandarTankov@gmail.com>
2014-11-08 12:37:45 +0200

    Add client h. and .cpp

--- client.cpp
+++ client.cpp
@@ -2,4 +2,62 @@
 
 Client::Client()
 {
+    patience_ = 0;
+    words_ = vector<string>();
+    level_ = 0;
 }
+
+Client::Client(patience, words, level)
+{
+    patience_ = patience;
+    words_ = words;
+    level_ = level;
+}
+
+Client::~Client()
+{
+
+}
+
+Client::Client(const Client& other)
+{
+
+}
+
+Client Client::operator=(const Client& other)
+{
+
+}
+
+size_t Client::get_patience() const
+{
+    return patience_;
+}
+
+vector<string> Client::get_words() const
+{
+    return words_;
+}
+
+size_t Client::get_level() const
+{
+    return level_;
+}
+
+void Client::set_patience(const size_t patience);
+void Client::set_words(const vector<string> words);
+void Client::set_level(const size_t level);
+
+void clear()
+{
+
+}
+
+void copy(const Client& other)
+{
+    patience_ = other.get_patience();
+    words_ = other.get_words();
+
+}
+
+void Client::generate_order(Factory facory_for_order);
--- client.h
+++ client.h
@@ -7,4 +7,39 @@ public:
     Client();
 };
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include "factory.h"
+
+using namespace std;
+
+class Client
+{
+public:
+    Client();
+    Client(patience, words, level);
+    ~Client();
+    Client(const Client& other);
+    Client operator=(const Client& other);
+
+    size_t get_patience() const;
+    vector<string> get_words() const;
+    size_t get_level() const;
+
+    void set_patience(const size_t patience);
+    void set_words(const vector<string> words);
+    void set_level(const size_t level);
+
+    void clear();
+    void copy(const Client);
+
+    void generate_order(Factory facory_for_order);
+
+private:
+    size_t patience_;
+    vector<string> words_;
+    size_t level_;
+};
+
 #endif // CLIENT_H
