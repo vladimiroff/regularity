67323b9ca14b2f09471ae05ed6eedc280b67b4b0
Alexandar <AlexandarTankov@gmail.com>
2014-11-08 12:16:50 +0200

    Add methods to worker header

--- worker.h
+++ worker.h
@@ -1,10 +1,36 @@
 #ifndef WORKER_H
 #define WORKER_H
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include "factory.h"
+
+using namespace std;
+
 class Worker
 {
 public:
     Worker();
+    Worker(patience, words, level);
+    ~Worker();
+    Worker(const Worker& other);
+    Worker operator=(const Worker& other);
+
+    size_t get_patience() const;
+    vector<string> get_words() const;
+    size_t get_level() const;
+
+    void set_patience(const size_t patience);
+    void set_words(const vector<string> words);
+    void set_level(const size_t level);
+
+    void generate_order(Factory facory_for_order);
+
+ private:
+    size_t patience_;
+    vector<string> words_;
+    size_t level_;
 };
 
 #endif // WORKER_H
