cbf550cfb70e3239d12425f6f17d2816af45dad4
Alexander Vakanin <vakanin.alexander@abv.bg>
2014-11-08 14:50:23 +0200

    update order

--- order.cpp
+++ order.cpp
@@ -3,5 +3,10 @@
 Order::Order(std::vector<std::string> words, Status status, std::size_t experience, std::size_t price, Client client,
              std::vector<Part> solution = vector<Part>())
 {
-
+    words_ = words;
+    status_ = status;
+    experience_ = experience;
+    price_ = price;
+    client_ = client;
+    solution_ = solution;
 }
