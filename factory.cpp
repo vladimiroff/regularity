a81fa347a5a9b8ecee5debf7a3e23bef1ccf1229
Alexander Vakanin <vakanin.alexander@abv.bg>
2014-11-09 02:33:33 +0200

    modified factory, add map<id,Order>

--- factory.cpp
+++ factory.cpp
@@ -4,8 +4,9 @@
 
 
 Factory::Factory(Storage* storage, std::size_t money, float rating, std::size_t level, std::size_t factory_experience,
-                 std::size_t level_experience, std::vector<Worker*> workers = std::vector<Worker*>(),
-                 std::vector<Order> orders = std::vector<Order>())
+                 std::size_t level_experience, std::size_t current_order_id = 0,
+                 std::vector<Worker*> workers = std::vector<Worker*>(),
+                 std::map<std::size_t, Order> orders = std::map<std::size_t, Order>())
 {
        workers_ = workers;
        orders_ = orders;
@@ -15,6 +16,7 @@ Factory::Factory(Storage* storage, std::size_t money, float rating, std::size_t 
        level_ = level;
        factory_experience_ = factory_experience;
        level_experience_ = level_experience;
+       current_order_id_ = current_order_id;
 }
 
 // TODO create destructor to delete workers
@@ -36,15 +38,16 @@ void Factory::set_workers(std::vector<Worker *> workers)
 
 void Factory::add_order(Order order)
 {
-    orders_.push_back(order);
+    orders_.insert(std::pair<std::size_t, Order>(current_order_id_, order));
+    current_order_id_++;
 }
 
-void Factory::remove_order()
+void Factory::remove_order(std::size_t order_id)
 {
-    orders_.pop_back();
+    orders_.erase(order_id);
 }
 
-void Factory::set_orders(std::vector<Order> orders)
+void Factory::set_orders(std::map<std::size_t, Order> orders)
 {
     orders_ = orders;
 }
@@ -88,6 +91,11 @@ void Factory::set_rating(float new_rating)
     rating_ = new_rating;
 }
 
+void Factory::set_current_order_id(size_t id)
+{
+    current_order_id_ = id;
+}
+
 void Factory::experience_increase(std::size_t experience)
 {
     factory_experience_ += experience;
@@ -102,7 +110,7 @@ void Factory::create_order(std::vector<std::string> words, Client client, std::s
                            std::string solution = "")
 {
     Order new_order(words, SENT, price, experience, client, solution);
-    orders_.push_back(new_order);
+    add_order(new_order);
 }
 
 Factory::~Factory()
@@ -119,7 +127,7 @@ std::vector<Worker*> Factory::get_workers()
     return workers_;
 }
 
-std::vector<Order> Factory::get_orders()
+std::map<std::size_t, Order> Factory::get_orders()
 {
     return orders_;
 }
@@ -148,3 +156,11 @@ std::size_t Factory::get_level_experience()
 {
     return level_experience_;
 }
+
+std::size_t Factory::get_current_id()
+{
+   return current_order_id_;
+}
+
+// public slots
+
--- factory.h
+++ factory.h
@@ -22,19 +22,21 @@ class Factory : public QObject
 public:
     virtual ~Factory();
     Factory(Storage *storage, std::size_t money, float rating, std::size_t level, std::size_t factory_experience,
-            std::size_t level_experience, std::vector<Worker*> workers, std::vector<Order> orders);
+            std::size_t level_experience, size_t current_order_id,
+            std::vector<Worker*> workers, std::map<std::size_t, Order> orders);
 
     void add_worker(Worker* new_worker);
     void remove_worker();
     void set_workers(std::vector<Worker*> workers);      //set_workers using vector
     void add_order(Order new_order);
-    void set_orders(std::vector<Order> orders);          //set_orders using vector
-    void remove_order();
+    void set_orders(std::map<size_t, Order> orders);          //set_orders using vector
+    void remove_order(std::size_t order_id);
     void add_money(std::size_t additional_money);
     void set_money(std::size_t money);                  //set_money
     void rating_increase(float additional_rating);
     void rating_decrease(float less_rating);
     void set_rating(float new_rating);                  //set_rating
+    void set_current_order_id(std::size_t id);
     void create_order(std::vector<std::string> words, Client client, std::size_t experience,
                       std::size_t price, std::string string);
     void experience_increase(std::size_t additional_experience);
@@ -44,29 +46,32 @@ public:
 
     // get methods
     std::vector<Worker*> get_workers();
-    std::vector<Order> get_orders();
+    std::map<size_t, Order> get_orders();
     std::size_t get_money();
     float get_rating();
     std::size_t get_level();
     std::size_t get_factory_experience();
     std::size_t get_level_experience();
+    std::size_t get_current_id();
 
 
 public slots:
-    QString add_money();
+    Q_INVOKABLE void add_money(size_t);
+  //  Q_INVOKABLE void levelUp();
 
 signals:
     void newOrder(QString);
 
 private:
-    std::vector<Worker*>        workers_;
-    std::vector<Order>          orders_;
-    std::unique_ptr<Storage>    storage_;
-    std::size_t                 money_;
-    float                       rating_;
-    std::size_t                 level_;
-    std::size_t                 factory_experience_;        //this is our xp
-    std::size_t                 level_experience_;          //this is xp you need for next level
+    std::vector<Worker*>            workers_;
+    std::map<std::size_t, Order>    orders_;
+    std::unique_ptr<Storage>        storage_;
+    std::size_t                     money_;
+    float                           rating_;
+    std::size_t                     level_;
+    std::size_t                     factory_experience_;        //this is our xp
+    std::size_t                     level_experience_;          //this is xp you need for next level
+    std::size_t                     current_order_id_;
 };
 
 
